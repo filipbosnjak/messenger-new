6ce24305e1b8493235783ed8aab6f0af411d31d4
Filip <filipo.bosnjak@gmail.com>
2023-12-18 15:45:58 +0100

    bugfix

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,6 +1,5 @@
 import { RegisterResponse } from "@/app/api/register/route";
 import { NextRequest, NextResponse } from "next/server";
-import messageServer from "@/message-server/server";
 
 export type MessageInput = {
   sender: string;
@@ -13,15 +12,6 @@ export async function POST(
   res: NextResponse<RegisterResponse>,
 ) {
   const message = (await req.json()) as MessageInput;
-  const { server, id } = messageServer;
-  server?.emit("message", "server server");
-
-  setTimeout(() => {
-    if (server !== null) {
-      console.log("message server is not null");
-    }
-    server?.emit("message", "server server");
-  }, 2000);
   return Response.json({
     message: "User created",
   });
